efaacbb17ddecf72f6877e8b28fd75800048da21
AMGuirra <Neto.0407@>
2023-11-16 20:38:47 -0400

    Craido banco. Vai ser feito login na pagina inicial, adicionar nome e vai salvar no banco com id incremental.

--- app/src/main/java/com/example/logingoogle/Pessoa.java
+++ app/src/main/java/com/example/logingoogle/Pessoa.java
@@ -1,18 +1,23 @@
 package com.example.logingoogle;
 
 public class Pessoa {
-
-    int id;
+    String id;
     String nome;
 
     public Pessoa() {
+        // Construtor vazio necessário para Firebase
     }
 
-    public int getId() {
+    public Pessoa(String id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
